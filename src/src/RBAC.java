102f8478e0aff75e8cc7d7445fa2b8e3d491bb5e
dbowling21 <davidbowling21@gmail.com>
2021-11-10 23:30:08 -0700

    created the method roleObjectMatrix() to take resourceObjects.txt and check it for duplicate objects, if no duplicates exist then the objects are added to an arraylist. This method then generates a role-object matrix using the new arraylist. Some additional helper methods were added such as roleSort to sort an array of roles and printMatrix to print a 2D array as a matrix.

--- src/src/RBAC.java
+++ src/src/RBAC.java
@@ -6,6 +6,7 @@ public class RBAC {
     public static BufferedReader input;
     public static ArrayList<String> asc = new ArrayList<String>(); //ascendants
     public static ArrayList<String> des = new ArrayList<String>(); //descendents
+    public static ArrayList<String> roles = new ArrayList<String>(); //descendents
 
     public static void main(String[] args) throws IOException {
         ArrayList<String> temp = new ArrayList<String>();
@@ -13,11 +14,11 @@ public class RBAC {
 
         confirm_LRH();
         //putting des into a hashset removes duplicates
-        HashSet uniqueDescendants = new HashSet();
-        uniqueDescendants.addAll(des);
+        HashSet rmDupes = new HashSet();
+        rmDupes.addAll(des);
         // put back into an arraylist and sort them
-        sortedDes.addAll(uniqueDescendants);
-        Collections.sort(sortedDes);
+        sortedDes.addAll(rmDupes);
+        sortedDes = roleSort(sortedDes);
 
         System.out.println("Role Hierarchy Top Down");
         for (String sortedDe : sortedDes) {
@@ -26,9 +27,14 @@ public class RBAC {
                     temp.add(asc.get(j));
                 }
             }
-            System.out.println(sortedDe + "--->" + temp);
+            System.out.println(sortedDe + "--->" + roleSort(temp));
             temp.clear();
         }
+        rmDupes.addAll(asc);
+        roles.addAll(rmDupes);
+        roles = roleSort(roles);
+        roleObjectMatrix();
+
 
     }
 
@@ -72,5 +78,79 @@ public class RBAC {
             confirm_LRH();
         }
 
+    }
+
+    public static void roleObjectMatrix() throws IOException {
+        ArrayList<String> resObj = new ArrayList<String>();
+        HashSet duplicates = new HashSet();
+        Scanner advance = new Scanner(System.in);
+        String obj;
+        boolean duplicate = false;
+
+        input = new BufferedReader(new FileReader("resourceObjects.txt"));
+
+        obj = input.readLine();
+        resObj.addAll(Arrays.asList(obj.split("\t")));
+        for (int i = 0; i < resObj.size(); i++) {
+            obj = resObj.get(i);
+            resObj.set(i, "~");
+            if (resObj.contains(obj)){
+                duplicate = true;
+                duplicates.add(obj);
+                //System.out.println("Duplicate object was found: " + obj);
+            }
+            resObj.set(i, obj); //restores the res array to its original form
+        }
+        input.close();
+        if (duplicate){
+            System.out.println("\nRemove these duplicates in another command line: "
+                    + duplicates + "\nPress ENTER to read it again or restart the program");
+            advance.nextLine();
+            resObj.clear();
+            roleObjectMatrix();
+        }
+
+        resObj.addAll(0, roles);
+        int rows = roles.size() + 1;
+        int cols = resObj.size() + 1;
+        String[][] ROM = new String[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            if (i >= 1){
+                ROM[i][0] = roles.get(i-1);
+            }
+            else {
+                ROM[0][0] = "  ";
+                for (int j = 0; j < cols; j++) {
+                    if (j >= 1){
+                        ROM[0][j] = "  " + resObj.get(j-1);
+                    }
+                }
+            }
+
+        }
+        System.out.println("\n");
+        printMatrix(ROM);
+
+
+    }
+
+    public static ArrayList<String> roleSort(ArrayList<String> roles){
+        Integer[] nums = new Integer[roles.size()];
+        for (int i = 0; i < roles.size(); i++) {
+            roles.set(i, roles.get(i).replace("R", ""));
+            nums[i] = Integer.valueOf(roles.get(i));
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            roles.set(i, "R" + nums[i]);
+        }
+        return roles;
+    }
+
+    public static void printMatrix(String matrix[][])
+    {
+        // Loop through all rows
+        for (String[] row : matrix)
+            System.out.println(Arrays.toString(row));
     }
 }
